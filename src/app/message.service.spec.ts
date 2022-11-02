31d81f7c46df62ef68666ae4eab8e4f2f3031dfe
Julen <julen@apolo.local>
2022-11-02 18:42:29 +0100

    Add isolated test to message service

---
+++ src/app/message.service.spec.ts
@@ -0,0 +1,19 @@
+import { MessageService } from "./message.service"
+
+describe('MessageService', () => {
+    let service: MessageService;
+
+    beforeEach(() => {
+        service = new MessageService();
+    })
+
+    it('should have no messages to start', () => {
+        expect(service.messages.length).toBe(0);
+    })
+
+    it('should add a messages when add is called', () => {
+        service.add('message1'); 
+
+        expect(service.messages.length).toBe(1);
+    })
+})
