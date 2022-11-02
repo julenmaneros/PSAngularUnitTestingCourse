5acb3ec93b49ccb1dbce5331900e255ee0ce9afa
Julen <julen@apolo.local>
2022-11-02 18:43:45 +0100

    Tell test whole history in each test

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -4,14 +4,17 @@ describe('MessageService', () => {
     let service: MessageService;
 
     beforeEach(() => {
-        service = new MessageService();
     })
 
     it('should have no messages to start', () => {
+        service = new MessageService();
+
         expect(service.messages.length).toBe(0);
     })
 
     it('should add a messages when add is called', () => {
+        service = new MessageService();
+
         service.add('message1'); 
 
         expect(service.messages.length).toBe(1);
