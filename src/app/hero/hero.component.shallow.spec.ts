4df28fb9f7d51c776453338ea63d9d6e646e1f0a
Julen <julen@apolo.local>
2022-11-04 18:28:12 +0100

    Use debugElement instead of nativeElement

--- src/app/hero/hero.component.shallow.spec.ts
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -1,5 +1,6 @@
 import { NO_ERRORS_SCHEMA } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
 import { HeroComponent } from "./hero.component";
 
 describe('HeroComponent (shallow tests)', () => {
@@ -25,7 +26,12 @@ describe('HeroComponent (shallow tests)', () => {
         // need to change detection to execute to implement those bindings a set values
         fixture.detectChanges();
         
+        // debugElement is a wrapper around the nativeElement
+        // does not make a difference here, but has a way to access other things, for example the access to the instance of the component the element belongs to
+        let deA = fixture.debugElement.query(By.css('a'));
+        expect(deA.nativeElement.textContent).toContain('SuperDude');
+
         // nativeElement exposes a DOM object, a regular browser DOM API 
-        expect(fixture.nativeElement.querySelector('a').textContent).toContain('SuperDude');
+        // expect(fixture.nativeElement.querySelector('a').textContent).toContain('SuperDude');
     });
 });
