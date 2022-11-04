a042412cec8cf609fab8ad59d3f5318bdb9e44aa
Julen <julen@apolo.local>
2022-11-04 18:12:01 +0100

    Add comment on NO_ERROR_SCHEMA

--- src/app/hero/hero.component.shallow.spec.ts
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { HeroComponent } from "./hero.component";
 
@@ -6,7 +7,8 @@ describe('HeroComponent (shallow tests)', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            declarations: [HeroComponent]
+            declarations: [HeroComponent],
+            schemas: [NO_ERRORS_SCHEMA] // Says to Angular not to try to validate the schema/template using
         });
         fixture = TestBed.createComponent(HeroComponent);
     });
